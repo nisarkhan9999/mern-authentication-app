227d0b794544688dfb7e68b302b8c792e95d3ecd
nisarlala0987-boop <nisarlala0987@gmail.com>
2026-08-30 13:00:17 +0500

    polish signup or login.ejs file

--- routes/singup.js
+++ routes/singup.js
@@ -2,26 +2,18 @@ import express from "express"
 const route = express.Router()
 import {signupUser,loginUser,userFind} from "../controller/userController.js"
 
+
 route.get('/signup',(req,res)=>{
     res.render("signup")
 })
 route.post("/signup",signupUser)
 
-
 route.get("/login",(req,res)=>{
     res.render("login")
 })
-
-
-
 route.post("/login",loginUser)
 route.get("/dashboard",userFind,(req,res)=>{
 res.send("welcome to dashboard")
 })
-
-
-
-
-
 
 export default route
